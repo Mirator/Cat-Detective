075f8906cab630b104f2dceff58e2dcf30f88bf1
Miroslav Pavelek <mirator.cz@gmail.com>
2025-01-25 13:23:51 +0100

    random final points!

--- Assets/Scripts/Managers/FinalLocationManager.cs
+++ Assets/Scripts/Managers/FinalLocationManager.cs
@@ -5,12 +5,33 @@ public class FinalLocationManager : MonoBehaviour
 {
     private Location finalLocation;
 
+    /// <summary>
+    /// Sets the final location from a list of possible locations.
+    /// If the final location is already set, it won't change.
+    /// </summary>
+    /// <param name="locations">The list of locations to choose from.</param>
     public void SetFinalLocation(List<Location> locations)
     {
-        finalLocation = locations[locations.Count - 1]; // Set the last location in the sequence
+        if (finalLocation != default)
+        {
+            Debug.Log($"Final location is already set to: {finalLocation}. Skipping.");
+            return;
+        }
+
+        if (locations == null || locations.Count == 0)
+        {
+            Debug.LogError("No locations available to set as the final location.");
+            return;
+        }
+
+        // Randomly select the final location
+        finalLocation = locations[Random.Range(0, locations.Count)];
         Debug.Log($"Final location set: {finalLocation}");
     }
 
+    /// <summary>
+    /// Gets the final location.
+    /// </summary>
     public Location GetFinalLocation()
     {
         return finalLocation;
